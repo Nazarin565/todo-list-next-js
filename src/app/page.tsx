f48518f2a398fadb2d8ae76b686e60cc9b4ce098
Nazar Oshchepkov <nazzarin565@gmail.com>
2024-12-11 23:28:45 +0200

    delete unused jsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,6 @@ export default async function Home() {
   return (
     <div className="p-12 flex justify-center">
       <main className="my-auto bg-white rounded-lg shadow-lg p-14 max-w-[800px] w-full flex flex-col gap-2">
-        <h1>Hello</h1>
         <DisplayUserName />
 
         <TodoList />
